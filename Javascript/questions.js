69d09ae7357045db03300a4ebfa51a589bbdd8b4
Ayodisu <disu.abdulwahab@gmail.com>
2022-10-04 11:02:33 +0100

    quiz

---
+++ Javascript/questions.js
@@ -0,0 +1,63 @@
+
+let questions = [
+    {
+        numb: 1,
+        questions: 'What does HTML stand for?',
+        answer: 'Hyper Text Markup Language',
+        options: [
+            'Hyper Text Preprocessor',
+            'Hyper Text Markup Language',
+            'Hyper Text Multiple Language',
+            'Hyper Tool Multi Language'
+        ]
+    },
+
+    {
+        numb: 2,
+        questions: 'What does CSS stand for?',
+        answer: 'Cascading Style Sheet',
+        options: [
+            'Common Style Sheet',
+            'Colorful style Sheet',
+            'Computer Style Sheet',
+            'Cascading Style Sheet'
+        ]
+    },
+
+    {
+        numb: 3,
+        questions: 'What does PHP stand for?',
+        answer: 'Hypertext Preprocessor',
+        options: [
+            'Hypertext Preprocessor',
+            'Hypertext Programming',
+            'Hypertext Preprogramming',
+            'Hometext Preprocessor'
+        ]
+    },
+
+    {
+        numb: 4,
+        questions: 'What does SQL stand for?',
+        answer: 'Structured Query Language',
+        options: [
+            'Stylish Question Language',
+            'Stylesheet Query Language',
+            'Statement Query Language',
+            'Structured Query Language'
+        ]
+    },
+
+    {
+        numb: 5,
+        questions: 'What does XML stand for?',
+        answer: 'eXtensible Markup Language',
+        options: [
+            'eXtensible Markup Language',
+            'eXecutable Multiple Language',
+            'eXtra Multi-Program Language',
+            'eXamine Multiple Language'
+        ]
+    },
+];
+
